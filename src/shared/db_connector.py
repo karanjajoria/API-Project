c75d840edc50fa689de047c4c245f03993d96c8f
karanjajoria <karanjajoria.965@gmail.com>
2026-09-09 16:26:22 +0530

    Can now perform SQL injections directly from db_connector.py

--- src/shared/db_connector.py
+++ src/shared/db_connector.py
@@ -8,7 +8,16 @@ class database():
         except sql.Error as e:
             print(f"Error connecting to database: {e}")
 
-    def execute_query(self,query:str):
-        cursor = self.connector.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+    def execute_query(self,query:str | None=None):
+        if query == None:
+            print("Nothing was passed in the database.execute_query function.")
+
+        try:
+            cursor = self.connector.cursor()
+            cursor.execute(query)
+            if query.lower().startswith("select"):
+                return cursor.fetchall()
+            return f"{query} executed successfully"
+        
+        except Exception as e:     
+            print(f"[ERROR] An error occured while working with database.execute_query as {e}")
